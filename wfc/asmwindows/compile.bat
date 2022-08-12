1604899ee647227d576201523b3b54368f42b3d3
ckor <admin.adj@gmail.com>
2022-08-13 01:30:18 +0200

    Update compile.bat

--- wfc/asmwindows/compile.bat
+++ wfc/asmwindows/compile.bat
@@ -1,2 +1,3 @@
-ml64 /c main.asm
-link /SUBSYSTEM:CONSOLE /MACHINE:X64 /LARGEADDRESSAWARE main.obj
+CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+CALL ml64 /c main.asm
+link /SUBSYSTEM:CONSOLE /MACHINE:X64 /LARGEADDRESSAWARE /NODEFAULTLIB /OPT:REF /OPT:ICF /TLBID:1 "libcmt.lib" "libvcruntime.lib" "libucrt.lib" "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" main.obj
