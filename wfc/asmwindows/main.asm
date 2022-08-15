3dcfd26bc067d0b9bf67348ccbba5ac799748040
FabinoHq <fabino.hq@gmail.com>
2022-08-15 10:33:40 +0200

    Update windows asm

--- wfc/asmwindows/main.asm
+++ wfc/asmwindows/main.asm
@@ -1,29 +1,82 @@
 ; External functions
-EXTRN _kbhit:PROC
-EXTRN _getch:PROC
-EXTRN _cputs:PROC
-EXTRN printf:PROC
+EXTRN _kbhit:PROC       ; _kbhit (Keyboard key press)
+EXTRN _getch:PROC       ; _getch (Get last pressed key)
+EXTRN putchar:PROC      ; putchar (Output one character to terminal)
+
+EXTRN ??_U@YAPEAX_K@Z:PROC      ; new[]
+EXTRN ??_V@YAXPEAX@Z:PROC       ; delete[]
+
+PUBLIC main
 
 ; Data segment
 .data
-    message db "string", 13, 0
+    message db "Hello World!", 0
 
 ; Code segment
 .code
+_TEXT SEGMENT
 main proc
+    ; Program start
+    xor rax, rax    ; Clear rax
+    xor rbx, rbx    ; Clear rbx
+    xor rcx, rcx    ; Clear rcx
+    sub rsp, 40     ; Push stack
 
-    sub rsp, 40h
+    ; Allocate memory
+    mov ecx, 4096           ; 4096 bytes
+    call ??_U@YAPEAX_K@Z    ; new[] (size in ecx, return address in rax)
+    push rax                ; Push address into stack
 
-    call _kbhit
-    call _getch
+    ; Wait for keyboard input
+    inputchar:
+        sub rsp, 40         ; Push stack
+        call _kbhit         ; Call _kbhit
+        add rsp, 40         ; Pop stack
+        test eax, eax       ; Set ZF to 1 if eax is equal to 0
+        je inputchar        ; Loop if _kbhit returned 0
 
-    mov rcx, offset message
-    call printf
+    ; Get character in al
+    sub rsp, 40         ; Push stack
+    call _getch         ; Call _getch
+    add rsp, 40         ; Pop stack
 
-    xor eax, eax
+    ; Move character into cl
+    mov cl, al
 
-    add rsp, 40h
-    ret 0
+    ; Output character from cl
+    sub rsp, 40     ; Push stack
+    call putchar
+    add rsp, 40     ; Pop stack
+
+    ; Output new line
+    mov cl, 10
+    sub rsp, 40     ; Push stack
+    call putchar
+    add rsp, 40     ; Pop stack
+
+    ; Output string
+    mov rbx, OFFSET message     ; Move address of message into rbx
+    outputstring:
+        mov cl, [rbx]           ; Move character into cl
+
+        sub rsp, 40             ; Push stack
+        call putchar            ; Output current character
+        add rsp, 40             ; Pop stack
+
+        inc rbx                 ; Increment rbx
+        mov al, [rbx]           ; Move next character into al
+        test al, al             ; Set ZF to 1 if al is equal to 0
+        jne outputstring        ; Loop if next character is not 0 (nul)
+
+    ; Cleanup memory
+    pop rcx                     ; Restore address from stack
+    call ??_V@YAXPEAX@Z         ; delete[] (address in rcx)
+
+    ; End of program
+    xor rax, rax    ; Clear rax
+    add rsp, 40     ; Pop stack
+    ret 0           ; Return 0
 
 main endp
+_TEXT ENDS
 end
