10b261659ecb26c573ad808a4fa4a43f5d522bfa
FabinoHq <fabino.hq@gmail.com>
2022-08-18 00:04:54 +0200

    Add WF I/O mode

--- wfc/asmwindows/main.asm
+++ wfc/asmwindows/main.asm
@@ -36,13 +36,16 @@ PUBLIC main
 .data
     message db "Hello World!", 00h
     file db "test.txt", 00h
-    mode_r db "r", 00h
-    mode_w db "w", 00h
 
-    std_handle dq 00h
-    input_file dq 00h
-    output_file dq 00h
-    rw_file dq 00h
+    std_handle dq 00h       ; Std output handle
+    input_file dq 00h       ; Input file handle
+    output_file dq 00h      ; Output file handle
+    rw_file dq 00h          ; R/W file handle
+    file_io dq 00h          ; File I/O mode
+
+    file_mode_r db "r", 00h     ; File mode read parameter
+    file_mode_w db "w", 00h     ; File mode write parameter
+    file_mode_rw db "a+", 00h   ; File mode R/W parameter
 
 ; Code segment
 .code
@@ -74,7 +77,7 @@ WFMain:
     push rax                ; Push address into stack
 
     ; Set memory pointer
-    mov r10, rax        ; Move address into r10
+    mov r10, rax        ; Move memory address into r10
     add r10, 33554432   ; Add memory offset ((16777216*4)/2 bytes)
 
     ; Get std output handle
@@ -83,11 +86,11 @@ WFMain:
     mov ecx, -11    ; 0FFFFFFF5H (-11) (STD_OUTPUT_HANDLE)
     call qword ptr __imp_GetStdHandle   ; Get std handle in rax
     add rsp, 40     ; Pop stack
-    mov [std_handle], rax    ; Store std handle
+    mov [std_handle], rax   ; Store std handle
 
     mov r11, rsp    ; Store main rsp into r11
 
-    xor r12, r12    ; Set iomode to standard mode
+    xor r12, r12    ; Set I/O mode to standard mode
 
     xor eax, eax    ; Clear eax  :  Register 
     xor ebx, ebx    ; Clear ebx  :  Back register
@@ -95,7 +98,7 @@ WFMain:
     xor edx, edx    ; Clear edx  :  Back pointer
 
     ; Read character into al
-    ;call WFKeyboardInput
+    ;call WFStandardInput
 
     ; Output character from al
     ;call WFStandardOutput
@@ -129,7 +132,7 @@ WFMain:
     xor rax, rax                ; Clear rax
     mov input_file, rax         ; Clear file handle
     lea rdx, offset file        ; Load file string address
-    lea r8, offset mode_r       ; Load mode string address
+    lea r8, offset file_mode_r  ; Load mode string address
     ;lea r8, offset mode_w      ; Load mode string address
     lea rcx, input_file  ; Load file handle address
     ; Open file (handle in rcx, path str addr in rdx, mode str addr in r8)
@@ -193,15 +196,16 @@ WFMainEnd:
     call ??_V@YAXPEAX@Z         ; delete[] (address in rcx)
 
     ; Wait for keyboard input
-    call WFKeyboardInput
+    xor r12, r12    ; Set I/O mode to standard mode
+    call WFStandardInput
 
     ; End of program
     xor rax, rax    ; Clear rax
     add rsp, 40     ; Pop stack
     ret 0           ; Return 0
 
-; WFKeyboardInput : low level keyboard input
-WFKeyboardInput:
+; WFStandardInput : low level standard input
+WFStandardInput:
     push rbx        ; Push back register
     push rcx        ; Push pointer
     push rdx        ; Push back pointer
@@ -209,20 +213,36 @@ WFKeyboardInput:
     push r11        ; Push main esp
     push r12        ; Push iomode
 
-    ; Wait for keyboard input
-    inputchar:
-        sub rsp, 40         ; Push stack
-        call _kbhit         ; Call _kbhit
-        add rsp, 40         ; Pop stack
-        test eax, eax       ; Set ZF to 1 if eax is equal to 0
-        je inputchar        ; Loop if _kbhit returned 0
+    sub rsp, 40     ; Push stack
 
-    ; Get character in al
-    xor rax, rax        ; Clear rax
-    sub rsp, 40         ; Push stack
-    call _getch         ; Call _getch
-    add rsp, 40         ; Pop stack
-    and rax, 0FFh       ; Mask low byte
+    cmp r12, 0      ; Standard I/O mode
+    je WFStandardInputStd      ; Jump to standard input mode
+    cmp r12, 4      ; Input file I/O mode
+    je WFStandardInputFile     ; Jump to file input mode
+    cmp r12, 5      ; Output file I/O mode
+    je WFStandardInputEnd      ; No input
+
+    WFStandardInputStd:     ; Standard input mode
+
+        ; Wait for keyboard input
+        WFStandardInputChar:
+            call _kbhit         ; Call _kbhit
+            test eax, eax       ; Set ZF to 1 if eax is equal to 0
+            je WFStandardInputChar      ; Loop if _kbhit returned 0
+
+        ; Get character in al
+        xor rax, rax        ; Clear rax
+        call _getch         ; Call _getch
+        and rax, 0FFh       ; Mask low byte
+
+        jmp WFStandardInputEnd
+
+    WFStandardInputFile:    ; File input mode
+        jmp WFStandardInputEnd
+
+    WFStandardInputEnd:
+
+    add rsp, 40     ; Pop stack
 
     pop r12         ; Pop iomode
     pop r11         ; Pop main esp
@@ -243,12 +263,27 @@ WFStandardOutput:
     push r11        ; Push main esp
     push r12        ; Push iomode
 
-    xor rcx, rcx    ; Clear rcx
-    mov cl, al      ; Move register value into cl
     sub rsp, 40     ; Push stack
-    call putchar    ; Output character from cl
-    add rsp, 40     ; Pop stack
 
+    cmp r12, 0      ; Standard I/O mode
+    je WFStandardOutputStd      ; Jump to standard output mode
+    cmp r12, 4      ; Input file I/O mode
+    je WFStandardOutputEnd      ; No output
+    cmp r12, 5      ; Output file I/O mode
+    je WFStandardOutputFile     ; Jump to file output mode
+
+    WFStandardOutputStd:    ; Standard output mode
+        xor rcx, rcx    ; Clear rcx
+        mov cl, al      ; Move register value into cl
+        call putchar    ; Output character from cl
+        jmp WFStandardOutputEnd
+
+    WFStandardOutputFile:   ; File output mode
+        jmp WFStandardOutputEnd
+
+    WFStandardOutputEnd:
+
+    add rsp, 40     ; Pop stack
     pop r12         ; Pop iomode
     pop r11         ; Pop main esp
     pop r10         ; Pop memory address
@@ -271,15 +306,33 @@ WFSetCursorPosition:
 
     sub rsp, 40     ; Push stack
 
-    xor rdx, rdx    ; Clear rdx
-    mov dx, bx      ; Y cursor position
-    shl edx, 16     ; Shift Y position to high
-    mov dx, ax      ; X cursor position
+    cmp r12, 0      ; Standard I/O mode
+    je WFSetCursorStd           ; Jump to standard output mode
+    cmp r12, 4      ; Input file I/O mode
+    je WFSetCursorInputFile     ; Jump to file input mode
+    cmp r12, 5      ; Output file I/O mode
+    je WFSetCursorOutputFile    ; Jump to file output mode
 
-    ; Set cursor position
-    mov rcx, std_handle     ; Move handle in rcx
-    ; Std handle in rcx, Coords in edx (X : low 4bytes, Y : high 4bytes)
-    call qword ptr __imp_SetConsoleCursorPosition
+    WFSetCursorStd:     ; Standard I/O mode
+        xor rdx, rdx    ; Clear rdx
+        mov dx, bx      ; Y cursor position
+        shl edx, 16     ; Shift Y position to high
+        mov dx, ax      ; X cursor position
+
+        ; Set cursor position
+        mov rcx, std_handle     ; Move handle in rcx
+        ; Std handle in rcx, Coords in edx(X : low 4bytes, Y : high 4bytes)
+        call qword ptr __imp_SetConsoleCursorPosition
+
+        jmp WFSetCursorEnd
+
+    WFSetCursorInputFile:   ; File input mode
+        jmp WFSetCursorEnd
+
+    WFSetCursorOutputFile:  ; File output mode
+        jmp WFSetCursorEnd
+
+    WFSetCursorEnd:
 
     add rsp, 40     ; Pop stack
 
